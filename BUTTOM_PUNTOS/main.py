2f0f474f83f2f4d12b3414d42f023a10be50443f
JGuevara23 <105224518+JordanGuevara@users.noreply.github.com>
2025-05-16 15:58:34 -0500

    update

--- BUTTOM_PUNTOS/main.py
+++ BUTTOM_PUNTOS/main.py
@@ -7,6 +7,7 @@ from qgis.core import (
     QgsPointXY,
     QgsWkbTypes
 )
+from qgis.PyQt.QtGui import QIcon
 from PyQt5.QtCore import QVariant
 from PyQt5.QtWidgets import QAction
 from qgis.utils import iface
@@ -17,8 +18,10 @@ class ExtremosPredio:
     def __init__(self, iface):
         self.iface = iface
         self.action = None
+        self.icono_path = os.path.join(os.path.dirname(__file__), "icono_puntos.png")
 
     def initGui(self):
+        icono = QIcon(self.icono_path) if os.path.exists(self.icono_path) else QIcon.fromTheme("document-print")
         self.action = QAction("Mostrar/Ocultar extremos predio", self.iface.mainWindow())
         self.action.triggered.connect(self.mostrar_ocultar_puntos_extremos)
         self.iface.addToolBarIcon(self.action)
@@ -31,7 +34,7 @@ class ExtremosPredio:
     def mostrar_ocultar_puntos_extremos(self):
         nombre_capa = "extremos_predio"
         tolerancia = 10  # metros
-        nombre_capa_calles = "Ejes_viales"
+        nombre_capa_calles = "Ejes Viales"
 
         for layer in QgsProject.instance().mapLayers().values():
             if layer.name() == nombre_capa:
